0ea326da4cf743d0fba85f41bed7f2aac2059711
Jeevan Subedi <subjeevan@hotmail.com>
2023-08-25 08:20:28 +0545

    'querybuilder'

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,31 +3,38 @@ from .models import *
 from .form import *
 from django.core import serializers
 from django.http import JsonResponse
+from django.db.models.functions import TruncDate
+from django.db.models import Sum
+
 import json
 # Create your views here.
 
 def homepage(request):
     salesform=SalesForm()
-    salesData = Sales.objects.all()
-    sales_json = serializers.serialize('json', salesData)
-    sales_json=json.loads(sales_json)
+    salesData = Sales.objects.values('product__name').annotate(price=Sum('price')).order_by('product__name')
+    query = Sales.objects.values('product__type').annotate(price=Sum('price')).order_by('product__name').query
+    print("Query:", query)
+
+    # sales_json = serializers.serialize('json', salesData)
+    # sales_json=json.loads(sales_json)
     label = [ ]
     data = [ ]
-    for sales in salesData:
-        print(sales.date)
+    # for sales in salesData:
+    #     print(sales.dates)
 
-        label.append(sales.date)
-        data.append(sales.price)
+    #     label.append(sales.dates)
+    #     data.append(sales.price)
 
 
     # label=json.dumps(label)
     # data=json.dumps(data)
     context={ 
-        'sales_json':sales_json,
+        # 'sales_json':sales_json,
         'salesData':salesData,
         'salesform':salesform,
         'label':label,
         'j_data':data,
+        'query':query
     }
 
     if request.method=='POST':
